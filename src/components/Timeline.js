3827c026c3bed0e7772fb3a1d98dd0c63ab79912
Fernando Ghisi <fghisi@casan.com.br>
2017-10-06 13:10:19 -0300

    Performing login and loading user's timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,19 +8,19 @@ export default class Timeline extends Component {
     this.state = {fotos: []};
   }
 
-  componentDidMount(){
-    fetch('http://localhost:8080/api/public/fotos/alots')
-      .then(response => response.json())
-      .then(fotos => {
-        this.setState({fotos: fotos});
-      });
+  componentDidMount() {
+    fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+        .then(response => response.json())
+        .then(fotos => {
+            this.setState({fotos:fotos});
+        });
   }
 
   render() {
     return (
       <div className="fotos container">
         {
-          this.state.fotos.map(foto => <FotoItem foto={foto}/>)
+          this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto}/>)
         }
       </div>            
     );
--- src/index.js
+++ src/index.js
@@ -10,12 +10,18 @@ import Login from './components/Login';
 import { Router, Route, browserHistory } from 'react-router';
 //import registerServiceWorker from './registerServiceWorker';
 
+function verificaAutenticacao(nextState, replace) {
+  if (localStorage.getItem('auth-token') == null) {
+    replace('/');
+  }
+}
+
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
   (
     <Router history={browserHistory}>
       <Route path="/" component={Login}/>
-      <Route path="/timeline" component={App} />
+      <Route path="/timeline" component={App} onEnter={verificaAutenticacao}/>
     </Router>
   ),
   document.getElementById('root')
