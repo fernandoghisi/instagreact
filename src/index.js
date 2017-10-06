77aa395c6599238cd1377390870fe1dd4d5d4197
Fernando Ghisi <fghisi@casan.com.br>
2017-10-06 13:28:54 -0300

    Including not allowed message and creating logout route

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import './css/login.css';
 
 import App from './App';
 import Login from './components/Login';
+import Logout from './components/Logout';
 import { Router, Route, browserHistory } from 'react-router';
+import { matchPattern } from 'react-router/lib/PatternUtils';
 //import registerServiceWorker from './registerServiceWorker';
 
 function verificaAutenticacao(nextState, replace) {
   if (localStorage.getItem('auth-token') == null) {
-    replace('/');
+    replace('/?msg=você precisa estar logado para acessar o endereço');
   }
 }
 
@@ -22,6 +24,7 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={Login}/>
       <Route path="/timeline" component={App} onEnter={verificaAutenticacao}/>
+      <Route path="/logout" component={Logout}/>
     </Router>
   ),
   document.getElementById('root')
