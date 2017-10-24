aa0689addde3bf65d9509e414ce37d4e64a6549f
Fernando Ghisi <fernandoghisi@gmail.com>
2017-10-24 14:40:33 -0200

    Combining reducers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Timeline from './components/Timeline';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { timeline } from './reducers/timeline';
+import { notificacao } from './reducers/header';
 
-const store = createStore(timeline, applyMiddleware(thunkMiddleware));
+const reducers = combineReducers({ timeline, notificacao });
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 class App extends Component {
   render() {
     return (
       <div id="root">
         <div className="main">
-          <Header />
+          <Header store={store} />
           <Timeline login={this.props.params.login} store={store} />
         </div>
       </div>
